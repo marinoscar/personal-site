524c6cb53c4e28415e774722a2fd7a02646df30f
Oscar Marin <oscar@marin.cr>
2016-04-24 08:36:28 -0600

    added the ability to show a message to return to the blog

--- Website/site/main.js
+++ Website/site/main.js
@@ -1,19 +1,34 @@
-﻿var lang = {
+﻿var qs = {
+    get: function (name) {
+        var url = window.location.href;
+        name = name.replace(/[\[\]]/g, "\\$&");
+        var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+            results = regex.exec(url);
+        if (!results) return null;
+        if (!results[2]) return '';
+        return decodeURIComponent(results[2].replace(/\+/g, " "));
+    }
+}
+
+var lang = {
     register: function(){
         $('#btn-spanish').on('click', function () { lang.doSpanish(); });
         $('#btn-english').on('click', function () { lang.doEnglish(); });
+        $('#back-to-blog').on('click', function () { window.history.back(); });
     },
     doEnglish: function () {
         $('#english-content').removeClass('hidden').addClass('show');
         $('#spanish-content').removeClass('show').addClass('hidden');
         $('#btn-spanish').removeClass('active');
         $('#btn-english').addClass('active');
+        $('#back-to-blog').html('Return to the blog');
     },
     doSpanish: function () {
         $('#spanish-content').removeClass('hidden').addClass('show');
         $('#english-content').removeClass('show').addClass('hidden');
         $('#btn-english').removeClass('active');
         $('#btn-spanish').addClass('active');
+        $('#back-to-blog').html('Regresar al blog');
     },
     isSpanish: function () {
         var res = navigator.language || navigator.userLanguage;
@@ -21,9 +36,12 @@
     },
     init: function () {
         lang.register();
-        if (lang.isSpanish()) {
+        if (lang.isSpanish())
             lang.doSpanish();
-        }
+        else
+            lang.doEnglish();
+        if (qs.get('s') === 'blog')
+            $('#blog-message').removeClass('hidden');
     }
 }
 
